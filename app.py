3ef392c769f65846a11166210b82f1993b5956af
Unknown <mikeyredtiger22@gmail.com>
2018-03-31 13:03:30 +0100

    added weeks parameter to API

--- app.py
+++ app.py
@@ -7,8 +7,11 @@ CORS(app)
 
 @app.route("/")
 def hello():
-    return str(schedule.Schedule().generate_week_schedule())
+    return str(schedule.Schedule().generate_schedule())
 
+@app.route("/generate/<int:weeks>")
+def generate10(weeks):
+    return str(schedule.Schedule().generate_schedule(weeks))
 
 if __name__ == "__main__":
     app.run()
--- schedule.py
+++ schedule.py
@@ -2,26 +2,22 @@ import random
 
 
 class Schedule:
-    PEOPLE_NUM = 10
-    workers = []
+    WORKERS_AMOUNT = 10
+    SLOTS_IN_WEEK = 10  # 10 slots in weekly schedule
 
-    def __init__(self):
-        # generate worker list
-        for i in range(0, self.PEOPLE_NUM):
-            name = i+1
-            self.workers.append(name)
+    def generate_schedule(self, weeks=1):
+        schedules = []
+        for x in range(weeks):
+            schedules.append(self.generate_week_schedule())
+        return schedules
+
 
     def generate_week_schedule(self):
         schedule = []
-        non_selected_workers = list(self.workers)
-
-        # much quicker to get a new random number
-        # than to create a new list of eligible workers
-        for i in range(0, 10):  # 10 slots in weekly schedule
-
-            pos = random.randint(0, len(non_selected_workers)-1)
-            selected_worker = non_selected_workers[pos]
-            non_selected_workers.remove(selected_worker)
-            schedule.append(selected_worker)
+        items = list(range(1, self.WORKERS_AMOUNT + 1))
+        for x in range(0, self.SLOTS_IN_WEEK):
+            pos = random.randint(0, len(items)-1)
+            selected_item = items.pop(pos)
+            schedule.append(selected_item)
 
         return schedule
